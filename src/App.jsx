27c19b266b48d7fb870f41e3eed68a1903b22c5f
Mahdi <mahdi1nap@gmail.com>
2025-09-21 17:47:37 +0330

    create vite stucture

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function First(){
+  return(
+    <>
+    <div className="border-4 border-amber-700 rounded-2xl mt-14">
+    <h1 className="text-9xl text-center bg-blue-400 text-white py-4" >hello world</h1>
+    </div>
+    </>
+  )
+}
