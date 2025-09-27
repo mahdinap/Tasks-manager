dc8f6a0f823ff4f3a7795545fa8c56b256e6e9e2
Mahdi <mahdi1nap@gmail.com>
2025-09-27 11:31:36 +0330

    design puzzle cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 export default function First(){
   return(
-    <>
-    <div className="border-4 border-amber-700 rounded-2xl mt-14">
-    <h1 className="text-9xl text-center bg-blue-400 text-white py-4" >hello world</h1>
-    </div>
-    </>
+   <>
+   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mx-3 my-5  ">
+    <div className="lg:row-span-2 lg:col-span-2 bg-amber-500">1</div>
+    
+    <div className="bg-sky-950 h-40">2</div>
+    <div className=" bg-red-500 h-40">3</div>
+    
+    <div className="bg-green-500 h-40">4</div>
+    <div className="bg-yellow-500 h-40">5</div>
+    <div className="bg-lime-400 h-40">6</div>
+    
+    
+    <div className="bg-pink-700 h-full lg:row-span-2" >7</div>
+    <div className="bg-green-950 h-40 lg:col-span-2">8</div>
+    <div className=" bg-rose-700 h-40 ">9</div>
+    
+    <div className=" bg-sky-700 h-40 ">10</div>
+    <div className=" bg-lime-700 h-40 ">11</div>
+    
+   </div>
+
+   </>
   )
 }
