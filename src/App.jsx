00ceabc8761b8c35cdb0575211f6602042357ed1
Mahdi <mahdi1nap@gmail.com>
2025-09-29 01:02:22 +0330

    complete lg size layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 export default function First(){
   return(
    <>
-   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mx-3 my-5  ">
-    <div className="lg:row-span-2 lg:col-span-2 bg-amber-500">1</div>
-    
-    <div className="bg-sky-950 h-40">2</div>
-    <div className=" bg-red-500 h-40">3</div>
-    
-    <div className="bg-green-500 h-40">4</div>
-    <div className="bg-yellow-500 h-40">5</div>
-    <div className="bg-lime-400 h-40">6</div>
-    
-    
-    <div className="bg-pink-700 lg:h-full h-40 lg:row-span-2" >7</div>
-    <div className="bg-green-950 h-40 lg:col-span-2">8</div>
-    <div className=" bg-rose-700 h-40 ">9</div>
-    
-    <div className=" bg-sky-700 h-40 ">10</div>
-    <div className=" bg-lime-700 h-40 ">11</div>
+   <div className="lg:h-[100dvh]  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2 mx-3 py-4 grid-rows-4 ">
+    <div className="lg:col-span-2  h-full">
+      <img src="../public/1.jpg" className="object-cover rounded-3xl h-full w-full mb-5" />
+    </div>
+    <div className="bg-sky-950 h-full w-full lg:row-span-3 rounded-2xl">
+      <img src="/public/3.jpg" alt="" className="object-cover w-full h-full rounded-2xl"/>
+    </div>
+    <div className="h-full w-full lg:col-span-2 lg:row-span-3 rounded-2xl">
+      <img src="/public/2.jpg" className="object-cover w-full h-full rounded-2xl"/>
+    </div>
+    <div className="bg-stone-600 h-full lg:row-span-3 text-center rounded-2xl py-7"><h1 className="text-white font-bold">Apps:</h1></div>
+    <div className="bg-stone-400 h-full lg:row-span-3 text-center rounded-2xl py-7"><h6 className="text-white font-bold">To Do list</h6></div>
+    <div className="bg-stone-600 h-full rounded-2xl"><h1 className="text-start p-7 font-bold text-white">Devices</h1></div>
+    <div className=" bg-stone-400 h-full lg:col-span-2 rounded-2xl text-start"><h2 className="p-4 font-bold">Notes:</h2></div>
+  
     
    </div>
+   
+
+
 
    </>
   )
