4f3c632e2c91d5e7a6525d1b2c8c36a43bb447a5
Mahdi <mahdi1nap@gmail.com>
2025-10-08 10:51:11 +0330

    add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage(){
+    return(
+        <>
+        <h1 className="text-7xl text-stone-800">404</h1>
+        <h2 className="text-4xl text-stone-600" >Page Not Found!</h2>
+        
+        </>
+    )
+}
