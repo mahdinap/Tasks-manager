bcd0a40ef044139059dc314fc346542f83b748af
Mahdi <mahdi1nap@gmail.com>
2025-12-01 17:48:47 +0330

    add validation to login

--- src/pages/logIn.jsx
+++ src/pages/logIn.jsx
@@ -5,33 +5,39 @@ import Subtitle from "../components/subtitle";
 import Input from "../inputs";
 import axios from "axios";
 
+import {z} from "zod"
+import {useForm} from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 
+
+
+const schema=z.object({
+    "email":z.email("invalid email"),
+    "password":z.string().min(8,"must be at least 8 charcters")
+})
 
 
 
 export default function Login(){
     useEffect(()=>{
         document.title="Log in"
-    },[])
+    },[])    
 
-    const [info,setInfo]=useState({
-"email":"",
-"password":"",
-"remmber":false,
-})
+    const {
+        register,
+        handleSubmit,
+        formState:{errors},
+    }=useForm(({
+        resolver:zodResolver(schema),
+        mode:"onBlur"
+    }))
 
-function onChange(e){
-    const id=e.target.id
-    const value =e.target.type==="checkbox"?e.target.checked:e.target.value 
 
-    setInfo((prev=>({...prev,[id]:value})))
-    
-    }
-async function handler(event){
-        event.preventDefault()
+async function handler(data){
+        
         try{
-            const res= await axios.post("http://localhost:5000/signin",info)
+            const res= await axios.post("http://localhost:5000/signin",data)
             console.log(res);
             
         }catch(res){
@@ -52,11 +58,13 @@ async function handler(event){
         <div>
 
         <h1 className="text-sky-950  text-4xl text-center font-bold font-suse" >log in</h1>
-        <form className="flex flex-col gap-5 p-5" onSubmit={handler}>
-            <Input label="Email" placholder="Enter Your Email" type="email" value={info.email} onChange={onChange} id="email" className="p-2 shadow-md border border-stone-200 rounded-md"/>
-            <Input label="Password" placholder="" type="password" value={info.password} onChange={onChange} id="password" className="p-2 shadow-md border border-stone-200 rounded-md"/>
+        <form className="flex flex-col gap-2 p-5" onSubmit={handleSubmit(handler)}>
+            <Input label="Email" placholder="Enter Your Email" type="email"  id="email" className="p-2 shadow-md border border-stone-200 rounded-md" {...register("email")}/>
+            {errors.email && <p className="text-red-700">{errors.email.message}</p>}
+            <Input label="Password" placholder="" type="password"  id="password" className="p-2 shadow-md border border-stone-200 rounded-md" {...register("password")}/>
+            {errors.password && <p className="text-red-700">{errors.password.message}</p>}
             <div>
-            <Checkbox label='Remmber Password'  id="remmber" name="remmberPass" onChange={onChange} checked={info.remmber} />
+            <Checkbox label='Remmber Password'  id="remmber" name="remmberPass"  />
             
             </div>
             <Buttons label="Log In" className="bg-sky-900 text-white hover:bg-sky-950 rounded-md py-2"/>
